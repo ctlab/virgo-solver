d398f5e147a57faebd4ee4144d4424ff5282471b
Nikolai <n.poperechnij@gmail.com>
2021-11-22 15:51:31 +0300

    Prim-D: update version

--- src/main/java/ru/itmo/ctlab/virgo/Main.java
+++ src/main/java/ru/itmo/ctlab/virgo/Main.java
@@ -29,7 +29,7 @@ import java.util.Set;
 import static java.util.Arrays.asList;
 
 public class Main {
-    public static final String VERSION = "0.1.3";
+    public static final String VERSION = "0.1.4";
 
     private static void checkCplex() {
         PrintStream stdout = System.out;
